58af727f2e2f67d79d52b93a75f022ef713ced08
azzen <git@azzen.dev>
2024-03-25 15:50:18 +0100

    [lab02] update with precompute step

--- lab2-profiling/code/src/sobel.c
+++ lab2-profiling/code/src/sobel.c
@@ -9,15 +9,14 @@
 
 #define GAUSSIAN_KERNEL_SIZE 3
 #define SOBEL_KERNEL_SIZE 3
-#define SOBEL_BINARY_THRESHOLD 150 // in the range 0 to uint8_max (255)
+#define SOBEL_BINARY_THRESHOLD 22500 // 150 * 150
 
-
-#define CONVOLUTION_1D(img, kernel, position)                                                                          \
-	(img->data[position - img->width - 1] * kernel[0] + img->data[position - img->width] * kernel[1] +                 \
-	 img->data[position - img->width + 1] * kernel[2] + img->data[position - 1] * kernel[3] +                          \
+#define CONVOLUTION_1D(img, kernel, position, width)                                                                   \
+	(img->data[position - width - 1] * kernel[0] + img->data[position - width] * kernel[1] +                           \
+	 img->data[position - width + 1] * kernel[2] + img->data[position - 1] * kernel[3] +                               \
 	 img->data[position] * kernel[4] + img->data[position + 1] * kernel[5] +                                           \
-	 img->data[position + img->width - 1] * kernel[6] + img->data[position + img->width] * kernel[7] +                 \
-	 img->data[position + img->width + 1] * kernel[8])
+	 img->data[position + width - 1] * kernel[6] + img->data[position + width] * kernel[7] +                           \
+	 img->data[position + width + 1] * kernel[8])
 
 #define CONVOLUTION_CHAINED(cache, kernel, position, width)                                                            \
 	(cache[position - width - 1]->pixel_val[0] * kernel[0] + cache[position - width]->pixel_val[0] * kernel[1] +       \
@@ -26,9 +25,13 @@
 	 cache[position + width - 1]->pixel_val[0] * kernel[6] + cache[position + width]->pixel_val[0] * kernel[7] +       \
 	 cache[position + width + 1]->pixel_val[0] * kernel[8])
 
-#define IS_BORDER(img, position)                                                                                       \
-	(position < img->width || position > img->width * (img->height - 1) || position % img->width == 0 ||               \
-	 position % img->width == img->width - 1)
+#define IS_BORDER(position, width)                                                                            \
+	(position % width == 0 || position % width == width - 1)
+
+#define OFFSETS(width) 																							   \
+	{                                                                                                                  \
+		-width - 1, -width, -width + 1, -1, 0, 1, width - 1, width, width + 1                                              \
+	}
 
 #define PIXEL_BLACK 0
 #define PIXEL_WHITE 255
@@ -66,12 +69,16 @@ struct img_1D_t *edge_detection_1D(const struct img_1D_t *input_img) {
 void rgb_to_grayscale_1D(const struct img_1D_t *img, struct img_1D_t *result) {
 	uint8_t r, g, b;
 	size_t i;
+
+	size_t img_size = img->width * img->height * img->components;
+	size_t num_components = img->components;
+
 	LIKWID_MARKER_START("rgb_grayscale_1d");
-	for (i = 0; i < img->width * img->height * img->components; i += img->components) {
+	for (i = 0; i < img_size; i += num_components) {
 		r = img->data[i + R_OFFSET] * FACTOR_R;
 		g = img->data[i + G_OFFSET] * FACTOR_G;
 		b = img->data[i + B_OFFSET] * FACTOR_B;
-		result->data[i / img->components] = r + g + b;
+		result->data[i / num_components] = r + g + b;
 	}
 	LIKWID_MARKER_STOP("rgb_grayscale_1d");
 }
@@ -79,14 +86,29 @@ void rgb_to_grayscale_1D(const struct img_1D_t *img, struct img_1D_t *result) {
 void gaussian_filter_1D(const struct img_1D_t *img, struct img_1D_t *res_img, const uint16_t *kernel) {
 	const uint16_t gauss_ponderation = 16;
 	size_t i;
+	size_t j;
+
+	const size_t width = img->width;
+	const size_t height = img->height;
+	const size_t img_size = width * height;
+	const int32_t offsets[] = OFFSETS(width);
+
+	int32_t sum;
+
 	LIKWID_MARKER_START("gaussian_1d");
-	for (i = 0; i < img->width * img->height * img->components; ++i) {
-		if (IS_BORDER(img, i)) {
+	// skip bottom and top borders
+	for (i = width; i < img_size - width; ++i) {
+		if (IS_BORDER(i, width)) {
 			res_img->data[i] = img->data[i];
 			continue;
 		}
-		// apply gaussian filter
-		res_img->data[i] = CONVOLUTION_1D(img, kernel, i) / gauss_ponderation;
+
+		sum = 0;
+		for (j = 0; j < 9; ++j) {
+			sum += img->data[i + offsets[j]] * kernel[j];
+		}
+		res_img->data[i] = sum / gauss_ponderation;
+
 	}
 	LIKWID_MARKER_STOP("gaussian_1d");
 }
@@ -97,16 +119,27 @@ void sobel_filter_1D(const struct img_1D_t *img, struct img_1D_t *res_img, const
 	int16_t gx, gy;
 	double_t magnitude;
 
+	const size_t width = img->width;
+	const size_t height = img->height;
+	const size_t img_size = width * height;
+	const int32_t offsets[] = OFFSETS(width);
+
 	LIKWID_MARKER_START("sobel_1d");
-	for (i = 0; i < img->height * img->width * img->components; ++i) {
-		if (IS_BORDER(img, i)) {
+	for (i = width; i < img_size - width; ++i) {
+		if (IS_BORDER(i, width)) {
 			res_img->data[i] = img->data[i];
 			continue;
+		}		
+
+		gx = 0;
+		gy = 0;
+		for (size_t j = 0; j < 9; ++j) {
+			gx += img->data[i + offsets[j]] * h_kernel[j];
+			gy += img->data[i + offsets[j]] * v_kernel[j];
 		}
 
-		gx = CONVOLUTION_1D(img, h_kernel, i);
-		gy = CONVOLUTION_1D(img, v_kernel, i);
-		magnitude = sqrt(gx * gx + gy * gy);
+		magnitude = gx * gx + gy * gy;
+		
 		res_img->data[i] = magnitude > SOBEL_BINARY_THRESHOLD ? PIXEL_WHITE : PIXEL_BLACK;
 	}
 	LIKWID_MARKER_STOP("sobel_1d");
@@ -168,12 +201,18 @@ void gaussian_filter_chained(const struct img_chained_t *img, struct img_chained
 	struct pixel_t *cur_img_pixel, *cur_res_pixel;
 	size_t i;
 
+	const size_t width = img->width;
+	const size_t height = img->height;
+	const size_t img_size = width * height * img->components;
+	const int32_t offsets[] = OFFSETS(width);
+	int32_t sum;
+
 	cur_img_pixel = img->first_pixel;
 	cur_res_pixel = res_img->first_pixel;
 
 	LIKWID_MARKER_START("gaussian_chained");
 	// populate cache
-	for (i = 0; i < img->width * img->height * img->components; ++i) {
+	for (i = 0; i < img_size; ++i) {
 		cache[i] = cur_img_pixel;
 		cur_img_pixel = cur_img_pixel->next_pixel;
 	}
@@ -181,8 +220,8 @@ void gaussian_filter_chained(const struct img_chained_t *img, struct img_chained
 	cur_img_pixel = img->first_pixel;
 	cur_res_pixel = res_img->first_pixel;
 
-	for (i = 0; i < img->width * img->height * img->components; ++i) {
-		if (IS_BORDER(img, i)) {
+	for (i = width; i < img_size - width; ++i) {
+		if (IS_BORDER(i, width)) {
 			cur_res_pixel->pixel_val[0] = cur_img_pixel->pixel_val[0];
 			cur_img_pixel = cur_img_pixel->next_pixel;
 			cur_res_pixel = cur_res_pixel->next_pixel;
@@ -190,7 +229,12 @@ void gaussian_filter_chained(const struct img_chained_t *img, struct img_chained
 		}
 
 		// apply gaussian filter
-		cur_res_pixel->pixel_val[0] = CONVOLUTION_CHAINED(cache, kernel, i, img->width) / gauss_ponderation;
+		sum = 0;
+		for (size_t j = 0; j < 9; ++j) {
+			sum += cache[i + offsets[j]]->pixel_val[0] * kernel[j];
+		}
+
+		cur_res_pixel->pixel_val[0] = sum / gauss_ponderation;
 
 		cur_img_pixel = cur_img_pixel->next_pixel;
 		cur_res_pixel = cur_res_pixel->next_pixel;
@@ -206,12 +250,17 @@ void sobel_filter_chained(const struct img_chained_t *img, struct img_chained_t 
 
 	size_t i;
 
+	const size_t width = img->width;
+	const size_t height = img->height;
+	const size_t img_size = width * height;
+	const int32_t offsets[] = OFFSETS(width);
+
 	cur_img_pixel = img->first_pixel;
 	cur_res_pixel = res_img->first_pixel;
 
 	LIKWID_MARKER_START("sobel_chained");
 	// populate cache
-	for (i = 0; i < img->width * img->height * img->components; ++i) {
+	for (i = 0; i < img_size; ++i) {
 		cache[i] = cur_img_pixel;
 		cur_img_pixel = cur_img_pixel->next_pixel;
 	}
@@ -219,17 +268,22 @@ void sobel_filter_chained(const struct img_chained_t *img, struct img_chained_t 
 	cur_img_pixel = img->first_pixel;
 	cur_res_pixel = res_img->first_pixel;
 
-	for (i = 0; i < img->width * img->height * img->components; ++i) {
-		if (IS_BORDER(img, i)) {
+	for (i = width; i < img_size - width; ++i) {
+		if (IS_BORDER(i, width)) {
 			cur_res_pixel->pixel_val[0] = cur_img_pixel->pixel_val[0];
 			cur_img_pixel = cur_img_pixel->next_pixel;
 			cur_res_pixel = cur_res_pixel->next_pixel;
 			continue;
 		}
 
-		gx = CONVOLUTION_CHAINED(cache, h_kernel, i, img->width);
-		gy = CONVOLUTION_CHAINED(cache, v_kernel, i, img->width);
-		magnitude = sqrt(gx * gx + gy * gy);
+		gx = 0;
+		gy = 0;
+		for (size_t j = 0; j < 9; ++j) {
+			gx += cache[i + offsets[j]]->pixel_val[0] * h_kernel[j];
+			gy += cache[i + offsets[j]]->pixel_val[0] * v_kernel[j];
+		}
+
+		magnitude = gx * gx + gy * gy;
 		cur_res_pixel->pixel_val[0] = magnitude > SOBEL_BINARY_THRESHOLD ? PIXEL_WHITE : PIXEL_BLACK;
 
 		cur_img_pixel = cur_img_pixel->next_pixel;
--- lab2-profiling/report/report.md
+++ lab2-profiling/report/report.md
@@ -5,6 +5,10 @@ author: "Rayane Annen"
 lang: fr
 date: \today
 geometry: margin=2cm
+header-includes:
+    - \usepackage{bm}
+    - \usepackage{xcolor}
+    - \definecolor{cgreen}{RGB}{0, 120, 0}
 ---
 
 # Benchmarks 
@@ -20,3 +24,136 @@ Machine utilisée pour les tests :
     - Flags de compilation: `-O3 -g -Wall -fno-inline -DLIKWID_PERFMON`
     - Librairies: `stb`, `math.h` et `Likwid`
 
+# Analyse du code 
+
+Dans cette première partie, il est question de trouver emplacements dans le code qui prennent le plus de temps à calculer. J'ai identifié plusieurs endroits dans le code demandant des performances : 
+
+- Filtre niveau de gris (listes chaînées et tableaux 1D): `grayscale_1d` et `grayscale_chained`
+- Filtre gaussien (listes chaînées et tableaux 1D): `gaussian_1d` et `gaussian_chained`
+- Filtre sobel  (listes chaînées et tableaux 1D): `sobel_1d` et `sobel_chained`
+
+En effectuant un benchmark avec likwid voici les résultats obtenus pour les listes chaînées: 
+
+- Temps d'exécution total : 0.325850 secondes
+- Commande exécutée : 
+
+`sudo likwid-perfctr -C E:N:8 -g MEM_DP -m ./lab01 ../images/half-life.png ../images/out.png 2`
+
+|                     | Temps d'exécution [s] | MFLOPs/s | Operational Intensity |
+|---------------------|-----------------------|----------|-----------------------|
+| `grayscale_chained` | 0.086963 (26%)        | 144.1993 | 0.0152                |
+| `gaussian_chained`  | 0.141852 (43%)        | 0.0001   | 1.336258e-08          |
+| `sobel_chained`     | 0.095893 (29%)        | 43.5051  | 0.0035                |
+
+Voici le résultat pour les tableaux 1D
+
+- Temps d'exécution total : 0.117468
+- Commande exécutée : 
+
+`sudo likwid-perfctr -C E:N:8 -g MEM_DP -m ./lab01 ../images/half-life.png ../images/out.png 1`
+
+ 
+|                | Temps d'exécution [s] | MFLOP/s  | Operational Intensity |
+|----------------|-----------------------|----------|-----------------------|
+| `grayscale_1d` | 0.030708 (26%)        | 144.1993 | 0.5390                |
+| `gaussian_1d`  | 0.038637 (32%)        | 0.0001   | 1.172069e-06          |
+| `sobel_1d`     | 0.047003 (40%)        | 88.7568  | 1.544869e-06          |
+
+\clearpage
+
+# Amélioration des performances
+
+## Baseline
+
+Voici ci-dessous un tableau récapitulatif des tests effectués ainsi qu'un graphe des temps de traitements mesurés pour chacun des tests.
+
+Images d'entrées : 
+
+| Nom du fichier  | Dimensions [pixels]          | Nombre de composantes par pixel |
+|-----------------|------------------------------|---------------------------------|
+| `half-life.png` | $2000 \times 2090 = 4038000$ | 3 (8-bit RGB)                   |
+| `medalion.png`  | $1267 \times 919 = 1164373$  | 3 (8-bit RGB)                   |
+| `nyc.png`       | $1150 \times 710 = 816500$   | 3 (8-bit RGB)                   |
+
+Nombre de type : 
+
+- Tableau 1D : `1`
+- Liste chaînée : `2`
+
+Sur la base de ces deux variables (l'image et le type de structures de données) une matrice de tests a été effectuée. Chaque test est effectué 50 fois et le résultat gardé est la moyenne de toutes les runs.
+
+![Résultats du benchmark obtenu avec la machine de test (baseline).](figures/results-baseline.eps){width=60%}
+
+## Accélération du calcul de la norme du gradient
+
+Lorsqu'on applique le filtre sobel sur l'image on effectue en chaque point de l'image une convolution permettant d'approximer les gradients horizontaux ($\bm{G_x}$) et verticaux ($\bm{G_y}$). 
+
+Une fois les gradients calculés, on peut obtenir une approximation de la norme euclidienne du gradient :
+
+$$\bm{G} = \sqrt{\bm{G_x}^2 + \bm{G_y}^2}$$
+
+Ce dernier calcul est ensuite utilisé pour déterminer si la norme est plus grande qu'un certain seuil, selon les cas on affiche un pixel blanc ou noir.
+
+Le calcul de la norme peut être simplifié en comparant simplement les distances au carré : 
+
+$$\text{Couleur} = \begin{cases}
+    \text{blanc},\qquad & \text{si } \bm{G}^2 > \text{seuil} \times \text{seuil},\\
+    \text{noir},\qquad  & \text{sinon}
+\end{cases}$$
+
+Le code modifié résultant est le suivant :
+
+```c
+    magnitude = gx * gx + gy * gy;
+    res_img->data[i] = magnitude > SOBEL_BINARY_THRESHOLD ? PIXEL_WHITE : PIXEL_BLACK
+```
+
+Finalement voici les résultat obtenus
+
+- Temps d'exécution total (liste chaînées) : 0.323816 [s]  (amélioration par rapport au baseline: \textcolor{cgreen}{-0.62\%})
+- Temps d'exécution total (tableaux 1D) :   0.112129 [s] (\textcolor{cgreen}{-1.64\%})
+
+|                 | Temps d'exécution [s]                  | MFLOPs/s | Operational Intensity |
+|-----------------|----------------------------------------|----------|-----------------------|
+| `sobel_chained` | 0.093865 (\textcolor{cgreen}{-2.11\%}) | 44.4452  | 0.0035                |
+| `sobel_1d`      | 0.043430 (\textcolor{cgreen}{-7.6\%})  | 0.0004   | 1.544869e-06          |
+
+Globalement on remarque que la différence reste minime, toutefois localement pour le filtre sobel, particulièrement avec des tableaux unidimensionnels la différence se voit, en effet on constate une amélioration de 7.6%.
+
+# Optimisation des boucles `for`
+
+- Optimisation de la boucle
+  - Pas de passage sur les pixels des bords inférieurs et supérieurs lors de l'application des filtres
+Code modifiée de la manière suivante :
+```c
+for (i = width; i < img_size - width; ++i) {
+    // ...
+}
+```
+  - Simplification de la vérification si un pixel est en bordure (on ne vérifie plus que les bords latéraux)
+Code modifiée de la manière suivante :
+```c
+if (position % width == 0 || position % width == width - 1) {
+    // on border
+}
+```
+
+- Précalcule des offsets pour les convolutions:
+```c
+// ...
+const int32_t offsets[] = {-width - 1, -width, -width + 1, -1, 0, 1, width - 1, width, width + 1}
+// ...
+sum = 0;
+for (j = 0; j < 9; ++j) {
+    sum += img->data[i + offsets[j]] * kernel[j];
+}
+res_img->data[i] = sum / gauss_ponderation;
+//...
+```
+
+Retrait des autres invariants de boucles des boucles `for`:
+
+- Taille et dimensions de l'image
+
+
+![Résultats du benchmark obtenu avec la machine de test (après accélération sobel).](figures/results-after-precompute.eps){width=60%}
